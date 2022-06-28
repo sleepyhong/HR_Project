530d60cfb0954651c4d94716bc1f629c3742f2f6
jeffnzk <jchan7240@gmail.com>
2022-06-28 12:20:35 -0400

    Add files via upload

---
+++ server/models/UserAuth.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const UserAuth = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const UserA = mongoose.model('UserA', UserAuth)
+
+module.exports = UserA
